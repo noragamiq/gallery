7a223a1ac257a0ecf26136aa54fbc27f5c4c3951
Ilya <64003029+noragamiq@users.noreply.github.com>
2023-12-05 17:38:31 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,114 @@
+// Custom Scripts
+// Custom scripts
+function tabs(headerSelector, tabSelector, contentSelector, activeClass, display='flex') {
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabSelector),
+          content = document.querySelectorAll(contentSelector)
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none'
+        });
+        tab.forEach(item => {
+            item.classList.remove(activeClass)
+        });
+    }
+    function showTabContent(i = 0) {
+       content[i].style.display = display
+       tab[i].classList.add(activeClass)
+    }
+    hideTabContent()
+    showTabContent()
+    header.addEventListener('click', e => {
+        const target = e.target
+        if (target.classList.contains(tabSelector.replace(/\./, '')) || 
+        target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+            tab.forEach((item, i) => {
+                if ( target == item || target.parentNode == item ) {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            });
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs( '.tabs__header' ,'.tabs__header-item', '.tabs__content-item', 'active')
+
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 991.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+    const nav = document.querySelector('.nav')
+
+    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+    const breakpoint = 1
+    if (window.scrollY >= breakpoint) {
+        nav.classList.add('fixed__nav')
+    } else {
+        nav.classList.remove('fixed__nav')
+    }
+}
+window.addEventListener('scroll', fixedNav)
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+    trigger = document.querySelector(trigger),
+        modal = document.querySelector(modal),
+        close = document.querySelector(close)
+
+    const body = document.body
+
+    trigger.addEventListener('click', e => {
+        e.preventDefault()
+        modal.style.display = 'flex'
+        body.classList.add('locked')
+    });
+    close.addEventListener('click', () => {
+        modal.style.display = 'none'
+        body.classList.remove('locked')
+    });
+    modal.addEventListener('click', e => {
+        if (e.target === modal) {
+            modal.style.display = 'none'
+            body.classList.remove('locked')
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
+// ВТОРОЙ аргумент - класс самого модального окна.
+// ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
+bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
+
+
